ddea9b6704f8c5aa9b255a4d1fc02d770ba6bf02
kedud <vincent.dudek@gmail.com>
2021-02-04 11:22:46 +0100

    Add getRateCategory

--- src/Accountdatas.js
+++ src/Accountdatas.js
@@ -14,6 +14,7 @@ export default class Accountdatas {
     constructor(sellsy) {
         this.sellsy = sellsy;
     }
+    
     getTaxes = () => {
         return sellsy.api({
             method: "Accountdatas.getTaxes",
